4a0b000edda8856e86b840cf694c20b813dc9f36
Bohdan2k23 <130351031+Bohdan2k23@users.noreply.github.com>
2024-06-08 12:40:43 +0200

    test

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -5,5 +5,5 @@ export const countContacts = async () => {
   const contacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
   return contacts.length;
 };
-
+const hello = await countContacts();
 console.log(await countContacts());
