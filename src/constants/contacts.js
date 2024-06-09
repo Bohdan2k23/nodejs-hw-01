6b5c965866430cd4ae56ee2067de2f31c689ae96
Bohdan2k23 <130351031+Bohdan2k23@users.noreply.github.com>
2024-06-09 11:44:32 +0200

    fix

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,6 @@
-export const PATH_DB = 'src/db/db.json';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export const PATH_DB = join(__dirname, '../db/db.json');
